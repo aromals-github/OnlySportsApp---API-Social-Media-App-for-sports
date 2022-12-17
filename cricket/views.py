4f3941b3e273b8004854a9961bf2ad8b17fd1c6b
Aromal S <93247039+aromals-github@users.noreply.github.com>
2022-12-18 01:40:16 +0530

    added  post funtion - upload post

--- cricket/views.py
+++ cricket/views.py
@@ -1,4 +1,4 @@
-# from rest_framework import viewsets
+
 from .models import CricketPosts
 from .serializers import PostSerializer
 
@@ -6,25 +6,48 @@ from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework.permissions import IsAuthenticated
 
+from users.models import Profile,Accounts
 
 class CricketPostsUploadView(APIView):
     
-    queryset            = CricketPosts.objects.all()
-    serializer_class    = PostSerializer
-    permission_classes  = (AllowAny,)
+    queryset                = CricketPosts.objects.all()
+    serializer_class        = PostSerializer
+    authentication_classes  = (TokenAuthentication,)
+    permission_classes      = (IsAuthenticated,)
 
-    def get(self,*args,**kwargs):
-        
-        response = { 'msg': 'hello' }
-        return Response(response)
-    
     def post(self,request,*args,**kwargs):
         
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({'data': serializer.data},status = status.HTTP_201_CREATED)
-        else:
-            return Response({'errors':serializer.errors},status = status.HTTP_400_BAD_REQUEST)
+        try:
+            user_logged     = request.user
+            user_id         = user_logged.id
+
+            if Profile.objects.get(user = user_id):
+                profile= Profile.objects.get(user = user_id)
+                
+                if ('C' in profile.games ) and ('C' in request.data['context']):
+                    
+                    user_id_logged = Accounts.objects.get(id = user_id)
+                    user = CricketPosts.objects.create(user = user_id_logged)
+                    serializer = PostSerializer(user,data = request.data)
+                    
+                    if serializer.is_valid():
+                        
+                        serializer.save()
+                        return Response({'data': serializer.data},status = 
+                                        status.HTTP_201_CREATED)
+                    else:
+                        
+                        return Response({'errors':serializer.errors},status = 
+                                        status.HTTP_400_BAD_REQUEST)
+                else:
+                    
+                    response =({'message': 
+                        "You can't create a post related to cricket,with your current profile "})
+                    return Response(response,status = status.HTTP_403_FORBIDDEN) 
+            
+        except:
+            
+            response = ({'message': 'You are not logged in or you dont have an profile'})
+            return Response(status = status.HTTP_206_PARTIAL_CONTENT)
